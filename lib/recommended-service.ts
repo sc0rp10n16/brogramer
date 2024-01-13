930377b4b51edbcea4377553040f9c133d617fd9
Akshith Mysa <akshiyhmysa.physics@gmail.com>
2024-01-13 15:04:51 +0530

    solved hydration error

--- lib/recommended-service.ts
+++ lib/recommended-service.ts
@@ -3,7 +3,6 @@ import { getSelf } from "@/lib/auth-service";
 import { resolve } from "path";
 
 export const getRecommended = async () => {
-  await new Promise((resolve) => setTimeout(resolve, 5000));
   const users = await db.user.findMany({
     orderBy: {
       createdAt: "desc",
